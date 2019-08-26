36daa9f55782b3e51031ee314a7375006577c54f
phi161 <phi161@users.noreply.github.com>
2019-08-26 17:53:04 +0300

    Add an HTTP client that uses Moya to perform the actual requests

---
+++ Twist/HTTPClient.swift
@@ -0,0 +1,45 @@
+//
+//  HTTPClient.swift
+//  Twist
+//
+
+import Foundation
+import Moya
+
+struct HTTPClient {
+    
+    let provider: MoyaProvider<LastfmTarget>
+    
+    init(isStubbed: Bool = false, tokenProvider: TokenProvider = TokenProvider()) {
+        if isStubbed {
+            provider = MoyaProvider<LastfmTarget>(stubClosure: MoyaProvider.immediatelyStub)
+        } else {
+            provider = MoyaProvider<LastfmTarget>(plugins: [
+                LastfmAuthPlugin(token: tokenProvider.lastfmToken),
+                NetworkLoggerPlugin(verbose: true, responseDataFormatter: JSONResponseDataFormatter)
+                ])
+        }
+    }
+    
+    func requestTopTracks(_ user: String) {
+        provider.request(.topTracks(user: user)) { (result) in
+            switch result {
+            case let .success(value):
+                print(value)
+            case let .failure(error):
+                print(error)
+            }
+        }
+    }
+
+}
+
+private func JSONResponseDataFormatter(_ data: Data) -> Data {
+    do {
+        let dataAsJSON = try JSONSerialization.jsonObject(with: data)
+        let prettyData =  try JSONSerialization.data(withJSONObject: dataAsJSON, options: .prettyPrinted)
+        return prettyData
+    } catch {
+        return data
+    }
+}
