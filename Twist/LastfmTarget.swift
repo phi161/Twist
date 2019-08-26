66207ebf76bdf43e5bb11067c85f29149b34bc4a
phi161 <phi161@users.noreply.github.com>
2019-08-26 12:07:40 +0300

    Add a Moya Target for last.fm endpoints

---
+++ Twist/LastfmTarget.swift
@@ -0,0 +1,47 @@
+//
+//  LastfmTarget.swift
+//  Twist
+//
+
+import Foundation
+import Moya
+
+enum LastfmTarget {
+    case topTracks(user: String)
+}
+
+extension LastfmTarget: TargetType {
+    var baseURL: URL {
+        return URL(string: "http://ws.audioscrobbler.com")!
+    }
+    
+    var path: String {
+        return "2.0"
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Task {
+        var parameters = [
+            "format": "json",
+            "api_key": ""
+        ]
+        switch self {
+        case let .topTracks(user):
+            parameters["method"] = "user.gettoptracks"
+            parameters["user"] = user
+        }
+        return Task.requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+    
+    var sampleData: Data {
+        return Data(base64Encoded: "")!
+    }
+    
+}
