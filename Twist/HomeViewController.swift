270bc42a9a50ec51be1ae95a6a1ae08d46781c24
phi161 <phi161@users.noreply.github.com>
2019-09-01 15:59:01 +0300

    Update the view controller according to ReactorKits mechanism

--- Twist/HomeViewController.swift
+++ Twist/HomeViewController.swift
@@ -11,27 +11,42 @@ import RxSwift
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    
-    let disposeBag = DisposeBag()
-    var recommendationsInteractor: RecommendationsInteractorType
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
 
-    required init?(coder aDecoder: NSCoder) {
-        let httpClient = HTTPClient(isStubbed: true)
-        let userRepository = UserRepository(client: httpClient)
-        recommendationsInteractor = RecommendationsInteractor(userRepository: userRepository)
-        super.init(coder: aDecoder)
-    }
-    
+    var disposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setup()
+    }
+    
+    private func setup() {
+        title = NSLocalizedString("Twist", comment: "Home View: The title of the view")
+        textField.placeholder = NSLocalizedString("last.fm username", comment: "Home View: The placeholder text for the username input text field")
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+}
+
+extension HomeViewController: StoryboardView {
+
+    func bind(reactor: HomeReactor) {
+        button.rx.tap
+            .map { [unowned self] in HomeReactor.Action.requestTracks(user: self.textField.text ?? "")}
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
         
-        recommendationsInteractor
-            .tracks(user: "")
-            .bind(to: tableView.rx.items(cellIdentifier: "Cell")) { _, model, cell in
-                cell.textLabel?.text = model.debugDescription
+        reactor.state.map { $0.isLoading }
+            .bind(to: activityIndicatorView.rx.isAnimating)
+            .disposed(by: disposeBag)
+
+        reactor.state.map { $0.tracks }
+            .bind(to: tableView.rx.items(cellIdentifier: "Cell")) { _, track, cell in
+                cell.textLabel?.text = track.debugDescription
             }
             .disposed(by: disposeBag)
     }
+    
 }
