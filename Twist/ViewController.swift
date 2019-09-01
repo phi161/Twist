90dd61c882f67c90176523c2b5080b146ff00b9b
phi161 <phi161@users.noreply.github.com>
2019-09-01 12:33:27 +0300

    Render the results on a tableView using RxCocoa bindings

--- Twist/ViewController.swift
+++ Twist/ViewController.swift
@@ -4,11 +4,33 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let disposeBag = DisposeBag()
+    var recommendationsInteractor: RecommendationsInteractor
+
+    required init?(coder aDecoder: NSCoder) {
+        let httpClient = HTTPClient()
+        let userRepository = UserRepository(client: httpClient)
+        recommendationsInteractor = RecommendationsInteractor(userRepository: userRepository)
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        recommendationsInteractor
+            .tracks(user: "")
+            .bind(to: tableView.rx.items(cellIdentifier: "Cell")) { _, model, cell in
+                cell.textLabel?.text = model.debugDescription
+            }
+            .disposed(by: disposeBag)
     }
 }
