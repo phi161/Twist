6eece83242e8003a66c1efc115fc8040e39469b7
phi161 <phi161@users.noreply.github.com>
2019-09-01 16:00:08 +0300

    Inject the reactor to the home view controller
    This should be the responsibility of another object (coordinator? DI framework?), but for now we use the app delegate.

--- Twist/AppDelegate.swift
+++ Twist/AppDelegate.swift
@@ -12,8 +12,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        guard let homeViewController = window?.rootViewController as? HomeViewController else { fatalError("Unexpected root view controller") }
+        guard
+            let navigationViewController = window?.rootViewController as? UINavigationController,
+            let homeViewController = navigationViewController.topViewController as? HomeViewController
+        else { fatalError("Unexpected root view controller") }
+
+        let httpClient = HTTPClient(isStubbed: false)
+        let userRepository = UserRepository(client: httpClient)
+        let interactor = RecommendationsInteractor(userRepository: userRepository)
+        let homeReactor: HomeReactor = HomeReactor(interactor: interactor)
+        homeViewController.reactor = homeReactor
         
+
         return true
     }
 
